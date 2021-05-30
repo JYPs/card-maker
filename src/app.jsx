9698289016debc8dfd0f2d6d84c98dd7b46740ea
JYPs <qkrwndma5@naver.com>
2021-05-30 16:44:48 +0900

    add login component

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,26 @@
-import "./app.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   // api key 가 유효하지 않다라는 오류가 발생해서 App에서 확인해보니... undefined였음 --> 원인은 .env가 최상위에 있지 않고 service폴더 하위에 있었음...
   console.debug("key :: ", process.env.REACT_APP_FIREBASE_API_KEY);
-  return <Login authService={authService} />;
+  return (
+    <div className={styles.app}>
+      {/* <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker />
+          </Route>
+        </Switch>
+      </BrowserRouter> */}
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
